f0e251b5481d393c59a8b4b254aeb252c9a3b080
yi-hsien <yi-hsien@ucsb.edu>
2020-10-07 19:51:56 -0700

    Update tokenizer-test.py

--- tokenizer-test.py
+++ tokenizer-test.py
@@ -6,7 +6,7 @@ tokenizer = BertTokenizer.from_pretrained("hfl/chinese-bert-wwm")
 model = BertModel.from_pretrained("hfl/chinese-bert-wwm")
 
 test_string = ['駐']
-test_u = '"\udce9\udca7\udc90"'
+test_u = "\udce9\udca7\udc90"
 
 
 print(test_string)
