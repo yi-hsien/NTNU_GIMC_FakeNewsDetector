fab25d02db85f77b24ae760c766f01db48be1deb
yi-hsien <yi-hsien@ucsb.edu>
2020-10-07 20:44:00 -0700

    demo done update

--- demo/demo_json.py
+++ demo/demo_json.py
@@ -62,54 +62,43 @@ print("<br>")
 
 df = [title, content]
 
-#encoding
+###encoding###
 def encode_words(s):
   tokens = tokenizer.tokenize(s)
   #tokens.append('[SEP]')
   return tokenizer.convert_tokens_to_ids(tokens)
-'''
-title = tf.ragged.constant(encode_words(df[0]))
-content = tf.ragged.constant(encode_words(df[1]))
-
-cls = [tokenizer.convert_tokens_to_ids(['[CLS]'])]
-content = tf.concat([cls,content], axis=-1)
-content_tensor = contents_list.to_tensor()
-'''
-
 input_title = encode_words(df[0])
 input_content = encode_words(df[1])
-
 
 print("done encoding")
 print("<br>")
 
+###choose testing range: title or content or both###
 total_content = [ input_content ]
 print("content chose")
 print("<br>")
 
-
-
-
-
-
+###load model###
 test_model = tf.keras.models.load_model('/home/yi-hsien/ntnu/NTNU_GIMC_FakeNewsDetector/models/200928-1_model.h5')
-#test_model = tf.keras.models.load_model('/home/yi-hsien/ntnu/NTNU_GIMC_FakeNewsDetector/test.h5')
 print("model loaded")
 print("<br>")
 
-
+###make prediction###
 probability_model = tf.keras.Sequential([test_model,tf.keras.layers.Softmax()])
 predictions = probability_model.predict(total_content)
 print("prediction made")
 print("<br>")
-print(predictions)
-
-
 
 import numpy as np
 
-#print("real news_percentage={}, fake news_percentage={}".format(predictions[0],predictions[1]))
-print(np.argmax(predictions))
+print("real news_percentage={}, fake news_percentage={}".format(predictions[0],predictions[1]))
+result = np.argmax(predictions)
+
+print("此篇為假新聞之機率有: {}<br>".format(prediction[1]))
+if result == 1:
+  print("認定為假新聞")
+else:
+  print("認定為真新聞")
 
 
 
