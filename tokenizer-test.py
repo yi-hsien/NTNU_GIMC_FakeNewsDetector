2d4413100b4f60cdc74385b97451169223a92217
yi-hsien <yi-hsien@ucsb.edu>
2020-10-07 20:06:54 -0700

    Update tokenizer-test.py

--- tokenizer-test.py
+++ tokenizer-test.py
@@ -1,6 +1,10 @@
 ## best example of all
 test = '\udce9\udca7\udc90'
 
+print("original length")
+print(len(test))
+
+
 test = test.encode('utf-8', errors='surrogateescape').decode('utf-8')
 
 print(test)
@@ -19,30 +23,3 @@ print(len(test))
 
 
 
-
-
-#import tensorflow as tf
-#import tensorflow_datasets
-#from transformers import *
-
-#tokenizer = BertTokenizer.from_pretrained("hfl/chinese-bert-wwm")
-#model = BertModel.from_pretrained("hfl/chinese-bert-wwm")
-
-'''
-test_string = ['駐']
-test_u = r'"\udce9\udca7\udc90"'
-
-
-print(test_string)
-#print(test_u)
-
-import json
-
-test_u = json.loads(test_u)
-print(test_u)
-'''
-
-
-#import json
-
-#print(json.loads(test))
