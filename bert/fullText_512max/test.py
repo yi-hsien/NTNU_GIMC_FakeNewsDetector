a3cc5b11ab743e74532215ae51dfa263ac4ca2b2
yi-hsien <yi-hsien@ucsb.edu>
2020-12-06 19:21:56 -0800

    update

--- bert/fullText_512max/test.py
+++ bert/fullText_512max/test.py
@@ -1,7 +1,7 @@
 #this file is used to find how accurate a fine_tuned_bert model is to a specific news provider
 #remember to change this accordingly
 label_due_to_news_provider = 1
-range_due_to_dataset = 200
+range_due_to_dataset = 140
 
 #other things to change
 '''
@@ -59,7 +59,7 @@ probability_model = tf.keras.Sequential([tf.keras.layers.Softmax()])
 
 
 #load entire news data, and process input dict
-total_data = load_newsdata('/home/yi-hsien/ntnu/test_csv/qiqi200V2.csv')
+total_data = load_newsdata('/home/yi-hsien/ntnu/test_csv/mygopen.csv')
 
 print(total_data)
 
@@ -84,7 +84,7 @@ for samples in total_data['content']:
 
 import numpy as np
 import csv
-with open('/home/yi-hsien/ntnu/test_csv_results/qiqi200V2.csv', 'w', newline='') as myfile:
+with open('/home/yi-hsien/ntnu/test_csv_results/mygopen.csv', 'w', newline='') as myfile:
      wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
      for i in range(len(predictions)):
         wr.writerow(predictions[i])
