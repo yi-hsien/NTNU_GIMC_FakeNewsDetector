0c25e358c5fdbc47b81650beb651cdaaebd8422e
yi-hsien <yi-hsien@ucsb.edu>
2021-01-23 18:16:01 -0800

    update

--- bert/first512/test.py
+++ bert/first512/test.py
@@ -1,14 +1,16 @@
 #this file is used to find how accurate a fine_tuned_bert model is to a specific news provider
 #remember to change this accordingly
-label_due_to_news_provider = 0
-range_due_to_dataset = 200
 
+all_csv_names = ['apple_realtime200V1','apple_realtime200V2','central200V1','chinatimes200V1','chinatimes200V2','ettoday200V1',
+         'ettoday200V2','udn_realtime200V1','udn_realtime200V2','liberty200V1','liberty200V2','globalmilitary200V1',
+         'globalmilitary200V2','kknews200V1','kknews200V2','mission200V1','mission200V2','nooho200V1','nooho200V2','qiqi200V1',
+         'qiqi200V2','mygopen','TFC']      
+all_csv_labels = [0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1]
+all_csv_size = [200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,140,250]
+result_credibilities = []
 #other things to change
 '''
 model_path
-data_name
-data_result_name
-tokenizer_path
 '''
 import tensorflow as tf
 import tensorflow_datasets
@@ -45,7 +47,7 @@ def one_time_content_encode(content_string):
 
 
 ##declare path
-bert_model_path = '/home/yi-hsien/ntnu/fine_tuned_bert/bert_2'
+bert_model_path = '/home/yi-hsien/ntnu/fine_tuned_bert/bert_23'
 
 #import/set_up tokenizer and model
 tokenizer = BertTokenizer.from_pretrained("/home/yi-hsien/ntnu/bert_model_chinese_wwm_ext/publish")
@@ -54,31 +56,54 @@ probability_model = tf.keras.Sequential([tf.keras.layers.Softmax()])
 
 
 
-#load entire news data, and process input dict
-total_data = load_newsdata('/home/yi-hsien/ntnu/test_csv/apple_realtime200V1.csv')
-
-print(total_data)
-
-predictions = []
-
-for samples in total_data['content']:
-    processed_input = one_time_content_encode(samples)
-    temp = probability_model.predict(loaded_model(processed_input)[0])
-    predictions.append(temp)
-   
 
 
 import numpy as np
 import csv
-with open('/home/yi-hsien/ntnu/test_csv_results/apple_realtime200V1.csv', 'w', newline='') as myfile:
-     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-     wr.writerow(predictions)
 
 
-#check credibility
-accurate_count = 0
-for i in range(range_due_to_dataset):
-  if np.argmax(predictions[i][0]) == label_due_to_news_provider:
-    accurate_count+=1
+for names in range(len(all_csv_names)):
+    print("processing: {}".format(all_csv_names[names]))
+    CSV_PATH = '/home/yi-hsien/ntnu/test_csv/'+all_csv_names[names]+'.csv'
+    RESULT_PATH = '/home/yi-hsien/ntnu/test_csv_results/'+all_csv_names[names]+'.csv'
+    
+    #load entire news data, and process input dict
+    total_data = load_newsdata(CSV_PATH)
+    print(total_data)
+    
+    predictions = []
+    #prediction process
+    
+    for samples in total_data['content']:
+        processed_input = one_time_content_encode(samples)
+        temp = probability_model.predict(loaded_model(processed_input)[0])
+        predictions.append(temp)
 
-print("credibility rate is {}%".format(accurate_count/range_due_to_dataset*100))
+    with open(RESULT_PATH, 'w', newline='') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(predictions)
+
+
+
+
+
+    #check credibility
+    accurate_count = 0
+    total_count = 0
+    for i in range(all_csv_size[names]):
+        if np.argmax(predictions[i][0]) == all_csv_labels[names]:
+            accurate_count+=1
+        total_count += 1
+
+    print("total:{}".format(total_count))
+    print("credibility rate is {}%".format(accurate_count/all_csv_size[names]*100))
+    result_credibilities.append(accurate_count/all_csv_size[names]*100)
+
+print("total result-------------")
+for names in range(len(all_csv_names)):
+    print(all_csv_names[names]+": {:.2f}".format(result_credibilities[names]))
+
+
+
+
+
